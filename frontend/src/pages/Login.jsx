bbc76fe4c4b65c1c79170c1b82d09669dda2929f
Evgeni Nikolov <evgeni.nikolov9009@gmail.com>
2025-07-21 17:59:26 +0100

    Implement user registration functionality and update login UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 import "./pages_css/login.css";
 
 function Login() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("")
+	const [message, setMessage] = useState("");
+
+	const handleRegister = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await fetch("http://localhost:8080/api/users/register", {
+				method: "POST",
+				headers: { "Content-type": "application/json" },
+				body: JSON.stringify({ username, password }),
+			});
+			const text = await response.text();
+			if (response.ok) {
+				setMessage("Registration success");
+			} else {
+				setMessage(text);
+			}
+		} catch (err) {
+			setMessage("Registration failed: " + err.message);
+		}
+	};
+
 	return (
 		<>
 			<div className="login-container">
 				<h1>Crypto Trading Sim</h1>
 				<form className="login-form">
 					<h2>Login</h2>
-					<input type="text" placeholder="Username" />
-					<input type="password" placeholder="Password" />
+					<input type="text"
+						   placeholder="Username" value={username}
+						   onChange={e => setUsername(e.target.value)}
+					/>
+					<input type="password"
+						   placeholder="Password"
+						   value={password}
+						   onChange={e => setPassword(e.target.value)}
+					/>
 					<div className="btn-container">
 						<button type="submit">Login</button>
-						<button type="button">Register</button>
+						<button type="button" onClick={handleRegister}>
+							Register
+						</button>
 					</div>
+					{message && <p className="message">{message}</p>}
 				</form>
 			</div>
 		</>
--- src/main/java/com/evgeni/cryptotradingsim/controllers/UserController.java
+++ src/main/java/com/evgeni/cryptotradingsim/controllers/UserController.java
@@ -1,4 +1,30 @@
 package com.evgeni.cryptotradingsim.controllers;
 
+import com.evgeni.cryptotradingsim.entities.User;
+import com.evgeni.cryptotradingsim.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+
+@CrossOrigin("http://localhost:5173")
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "UserController is working";
+    }
+
+    @PostMapping("register")
+    public void registerUser(@RequestBody User user) throws SQLException {
+        userService.registerUser(user);
+    }
 }
